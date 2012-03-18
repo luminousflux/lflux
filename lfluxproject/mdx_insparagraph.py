6871e695fec03f4c76e64176a8b5ccbec91e16d3
fin <fin+github@xbhd.org>
2012-03-18 21:15:22 +0100

    sensible highlighting

--- lfluxproject/mdx_insparagraph.py
+++ lfluxproject/mdx_insparagraph.py
@@ -1,6 +1,7 @@
 import re
 from markdown import Extension
 from markdown.treeprocessors import Treeprocessor
+from markdown.preprocessors import Preprocessor
 from markdown import etree_loader
 
 __version__ = '0.1'
@@ -18,10 +19,34 @@ class InsParagraphProcessor(Treeprocessor):
                 self.run(child)
         return root
 
+class InsParagraphPreprocessor(Preprocessor):
+    """ move .ins declaration inside block "tags" """
+    def run(self, lines):
+        BLOCKCHARS = ('=','>','*','+','-','#',)
+        newlines = []
+        print lines
+        
+        for line in lines:
+            if line.startswith('.ins ') and len(line)>6 and (line[4] in BLOCKCHARS or line[5] in BLOCKCHARS):
+                idx = 5
+                while len(line) > idx+1 and line[idx] in BLOCKCHARS:
+                    idx+=1
+                newline = line[5:idx] + ' .ins ' + line[idx:]
+                print newline
+                newlines.append(newline)
+            else:
+                newlines.append(line)
+        print newlines
+        return newlines
+
+        
+
+
 class InsParagraphExtension(Extension):
     def extendMarkdown(self, md, md_globals):
         md.registerExtension(self)
         md.treeprocessors['insparagraph'] = InsParagraphProcessor(md)
+        md.preprocessors.insert(0,'insparagraph', InsParagraphPreprocessor(md))
 
 
 def makeExtension(configs=[]):
--- lfluxproject/settings.py
+++ lfluxproject/settings.py
@@ -86,6 +86,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -115,9 +116,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'lfluxproject.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
