0f7334d13d6ebb88289ec9ec509a06fe6da5830d
fin <fin+github@xbhd.org>
2012-03-22 23:12:51 +0100

    add a helpful wrapper for past model versions.

--- lfluxproject/lstory/models.py
+++ lfluxproject/lstory/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
+from ltools.managers import VersionManagerAccessor
 import reversion
 
 class Story(models.Model):
@@ -22,6 +23,8 @@ class Story(models.Model):
 
 
     tags = TaggableManager(blank=True)
+
+    versions = VersionManagerAccessor()
 
     def __unicode__(self):
         return "%s%s" % (self.title, ' /unpublished' if not self.published else '')
---
+++ lfluxproject/ltools/managers.py
@@ -0,0 +1,67 @@
+import reversion
+from django.db.models import signals
+
+
+''' convenience methods for reversion-backed models '''
+
+class VersionManagerHelperException(Exception):
+    pass
+
+
+class VersionManagerAccessor(object):
+    ''' accessor for history objects, inspired by
+        the Django Manager and (Generic)ForeignKey classes
+
+        returns a VersionManager for the object.
+    '''
+
+    class VersionManager(object):
+        def _translate_reversion_call(self, *args, **kwargs):
+            return args.insert(0, self.obj)
+
+        def _proxify_object(self, version):
+            ret = self.cls()
+            ret.__dict__.update(version.field_dict)
+            ret._version = version
+            return ret
+
+        def _proxify_object_s(self, retval):
+            ret = None
+            try:
+                for x in retval:
+                    ret = ret or []
+                    ret.append(self._proxify_object(x))
+            except TypeError, e:
+                ret = self._proxify_object(retval)
+            return  ret
+
+
+        def __init__(self, obj, cls):
+            self.obj = obj
+            self.cls = cls
+
+            methods = {'list': 'get_for_object',
+                       'for_date': 'get_for_date',
+                      }
+            for key,value in methods.iteritems():
+                setattr(self, key,
+                        lambda *args, **kwargs: self._proxify_object_s(
+                            getattr(reversion, value)(self.obj, *args, **kwargs)
+                            )
+                        )
+
+    def __get__(self, instance, owner):
+        if not instance:
+            return self
+        else:
+            return self.VersionManager(instance, owner)
+
+    def _ignore_versioned_modifications(self, instance, sender, **kwargs):
+        if instance and hasattr(instance, '_version'):
+            raise VersionManagerHelperException("you're trying to overwrite a former version of this model. sorry, that will not work out")
+
+
+    def contribute_to_class(self, cls, name):
+        setattr(cls,name, self)
+        signals.pre_delete.connect(self._ignore_versioned_modifications, sender=cls)
+        signals.pre_save.connect(self._ignore_versioned_modifications, sender=cls)
