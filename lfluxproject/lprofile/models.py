01b5bda141523716ccb8942cd7bc5cc68b54a0ab
fin <fin+github@xbhd.org>
2012-07-19 16:02:03 +0200

    fix install & test environment creation

--- lfluxproject/lprofile/models.py
+++ lfluxproject/lprofile/models.py
@@ -5,7 +5,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
-
+from django.db.models import get_apps
+from django.contrib.auth.management import create_permissions
 
 class Profile(UserenaBaseProfile):
     user = models.OneToOneField(User,
@@ -17,6 +18,11 @@ class Profile(UserenaBaseProfile):
 def demo_mode_set_permission(sender, instance, created, raw, **kwargs):
     Profile.objects.get_or_create(user=instance)
     g, created = Group.objects.get_or_create(name='editor')
+
+    # ensure that all Permissions exist. not doing it would break test environments & installation
+    for app in get_apps():
+           create_permissions(app, None, 2)
+
     default_group_perms = [
         Permission.objects.get_by_natural_key(app_label='lstory', model='story', codename='add_story'),
         Permission.objects.get_by_natural_key(app_label='lstory', model='story', codename='change_story'),
