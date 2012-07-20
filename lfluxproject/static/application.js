cd6f07d78a22782614850f5057a3f272ef9c2f61
fin <fin+github@xbhd.org>
2012-07-20 17:14:28 +0200

    make code readable by using jquery plugins

--- lfluxproject/static/application.js
+++ lfluxproject/static/application.js
@@ -2,10 +2,10 @@ function scrollTimelineToAppropriateDate() {
     var highlight_el = $('.timeline.highlight a[data-date="'+$('#article').data('article-previous-date')+'"]');
     var version_el = $('.timeline.version a[data-date="'+$('#article').data('article-version-date')+'"]');
     if(version_el.length>0 && version_el.offset().left!=0) {
-        $('.timeline.version').scrollLeft(version_el.offset().left - version_el.parents(':scrollable').first().offset().left);
+        $('.timeline.version').scrollTo(version_el);
     }
     if(highlight_el.length>0 && highlight_el.offset().left!=0) {
-        $('.timeline.highlight').scrollLeft(highlight_el.offset().left - highlight_el.parents(':scrollable').first().offset().left);
+        $('.timeline.highlight').scrollTo(highlight_el);
     }
 }
 
@@ -46,8 +46,10 @@ $(document).ready(initMarkAsRead);
 
 initTimelines_done = false;
 function initTimelines() {
-    if(initTimelines_done)
+    if(initTimelines_done) {
+        scrollTimelineToAppropriateDate();
         return;
+    }
     initTimelines_done = true;
     var width = $('.timeline li .month').parent().map(
             function() {
