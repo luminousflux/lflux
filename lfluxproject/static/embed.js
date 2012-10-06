4fa2871407926373f139386bebfdfe72767e8970
fin <fin+github@xbhd.org>
2012-10-06 19:30:53 +0200

    slightly better wording

--- lfluxproject/static/embed.js
+++ lfluxproject/static/embed.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     if(document.cookie.length==0) {
         $('body').addClass('noSession');
-        $('#mark_as_read').html('Luminous Flux usually saves when you last read each article, to highlight any changes when you next read it! Read the full version if you\'d like to use this feature!');
+        $('#mark_as_read').html('Luminous Flux usually saves when you last read each article, to highlight any changes when you next read it. Your browser does not allow this for embedded content.<br />Read the full version if you\'d like to use this feature!');
     }
 });
