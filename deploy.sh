88e6497b66aa8d04b4ed0b9f731f850a747d4556
fin <fin+github@xbhd.org>
2012-10-12 01:20:29 +0200

    first version of deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# this is an easy (re)deploy script for running a lflux instance w/ pbundler, gunicorn and supervisord
+
+cd ..
+pbundle # update dependencies.
+pbundle run ./manage.py collectstatic --noinput
+pbundle run ./manage.py migrate
+sudo supervisorctl restart `echo $SUPERVISOR_NAME`
+echo $SUPERVISOR_NAME
