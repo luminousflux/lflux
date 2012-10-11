8404db494364fbbc3340c014594e29abccb9b8c4
fin <fin+github@xbhd.org>
2012-10-12 01:23:37 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # this is an easy (re)deploy script for running a lflux instance w/ pbundler, gunicorn and supervisord
 
-cd ..
 pbundle # update dependencies.
 pbundle run ./manage.py collectstatic --noinput
 pbundle run ./manage.py migrate
