bc312bd0aa66c15a5297d253cecb94453a431e60
fin <fin+github@xbhd.org>
2012-10-12 01:39:15 +0200

    fix typo

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # this is an easy (re)deploy script for running a lflux instance w/ pbundler, gunicorn and supervisord
 
 pbundle # update dependencies.
-pbundle run ./lfuxproject/manage.py collectstatic --noinput
+pbundle run ./lfluxproject/manage.py collectstatic --noinput
 pbundle run ./lfluxproject/manage.py migrate
 X=`pbundle run printenv SUPERVISOR_NAME`
 pbundle run sudo supervisorctl restart $X
