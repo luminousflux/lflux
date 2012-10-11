6e1afc45af122e4bbce9962a1e5603e19faafdbb
fin <fin+github@xbhd.org>
2012-10-12 01:33:56 +0200

    debugging

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@
 pbundle # update dependencies.
 pbundle run ./manage.py collectstatic --noinput
 pbundle run ./manage.py migrate
+pbundle run printenv SUPERVISOR_NAME
 pbundle run sudo supervisorctl restart `printenv SUPERVISOR_NAME`
