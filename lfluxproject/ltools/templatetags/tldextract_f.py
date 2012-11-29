25bdc4f3066e68f6d72b6b686172e29781de0729
Markus Hametner <fin+github@xbhd.org>
2012-11-29 16:01:37 +0100

    remove debug

--- lfluxproject/ltools/templatetags/tldextract_f.py
+++ lfluxproject/ltools/templatetags/tldextract_f.py
@@ -4,5 +4,4 @@ register = template.Library()
 
 @register.filter('tldextract')
 def tldextract_filter(value):
-    print dir(tldextract)
     return ('.'.join([x for x in tldextract.extract(value) if x!='www'])).strip('.')
