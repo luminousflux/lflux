13135b5a5dd85cac27285784f91baf992be458e8
Markus Hametner <fin+github@xbhd.org>
2012-12-06 19:29:46 +0100

    added test for basic story creation, rendering and embedding

--- lfluxproject/tests/test_basics.py
+++ lfluxproject/tests/test_basics.py
@@ -1,5 +1,8 @@
 from django.test.client import Client
 from django.test import TestCase
+from lstory.models import Story
+from datetime import datetime
+import reversion
 
 
 class BasicFunctionalityTest(TestCase):
@@ -7,3 +10,11 @@ class BasicFunctionalityTest(TestCase):
         client = Client()
         response = client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_story_creation(self):
+        client = Client()
+        with reversion.create_revision():
+            Story.objects.create(published=datetime.now(), body='roflcopter', summary='roflcopter', title='roflcopter',slug='roflcopter',name='roflcopter')
+        responses = [client.get('/story/roflcopter/'),client.get('/story/roflcopter/embed/')]
+        for x in responses:
+            self.assertEqual(x.status_code, 200)
