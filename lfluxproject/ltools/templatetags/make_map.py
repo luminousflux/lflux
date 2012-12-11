269b58b5fb0ee6f60780b5271953589b124456a5
Markus Hametner <fin+github@xbhd.org>
2012-12-11 15:55:05 +0100

    change world map foreground color

--- lfluxproject/ltools/templatetags/make_map.py
+++ lfluxproject/ltools/templatetags/make_map.py
@@ -7,7 +7,7 @@ register = template.Library()
 def make_map(country, field_id):
     try:
         return mark_safe("""<script>
-                    WorldMap({'id': '%(field_id)s', padding: 0, zoom: '%(neighbors)s', detail: {'%(country)s': 'black'}});
+                    WorldMap({'id': '%(field_id)s', padding: 0, zoom: '%(neighbors)s', fgcolor: '#F1F1F1', detail: {'%(country)s': 'black'}});
                   </script>
                """ % {'country': country.ISO.lower(), 'field_id': field_id, 'neighbors': country.neighbours.lower().replace(',li','')}
                )
