d6daeb6742b65076dcc31a9d46334a17aa9168b6
Markus Hametner <fin+github@xbhd.org>
2012-12-11 15:55:49 +0100

    better deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
 # this is an easy (re)deploy script for running a lflux instance w/ pbundler, gunicorn and supervisord
 
 unset GIT_DIR
-bash ./update_dependencies.sh
-pbundle # update dependencies.
+bash ./update_dependencies.sh # pbundle updates are run in here too
 pbundle run ./lfluxproject/manage.py collectstatic --noinput
 pbundle run ./lfluxproject/manage.py syncdb
 pbundle run ./lfluxproject/manage.py migrate
