5c6499c40e855b97945160ed4b6ceeae837abb15
Markus Hametner <fin+github@xbhd.org>
2012-12-12 19:27:53 +0100

    better translations

--- lfluxproject/lqa/models.py
+++ lfluxproject/lqa/models.py
@@ -3,9 +3,10 @@ from django.contrib.comments.models import Comment
 from django.contrib.auth.models import User
 from lstory.models import Story
 from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_noop
 
-STATES = ['new', 'researching', 'answered']
-STATES = [(x,x,) for x in STATES]
+STATES = [ugettext_noop('new'), ugettext_noop('researching'), ugettext_noop('answered')]
+STATES = tuple([(x,_(x),) for x in STATES])
 
 class Question(models.Model):
     title = models.CharField(verbose_name=_('Title'), max_length=255)
@@ -15,7 +16,7 @@ class Question(models.Model):
 
     created_at = models.DateTimeField(verbose_name=_('Created at'), auto_now_add=True)
 
-    state = models.TextField(verbose_name=_('State'), max_length=255, choices=STATES, default=STATES[0])
+    state = models.TextField(verbose_name=_('State'), max_length=255, choices=STATES, default=STATES[0][0])
 
     @models.permalink
     def get_absolute_url(self):
